2f624138627a701f0074fe9f380a10a98ac093dd
nwre87 <nwre87@gmail.com>
2017-07-25 12:43:53 -1000

    add passport

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const next = require('next');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 const dev = process.env.NODE_ENV !== 'production';
 
 const db = require('./server/models');
+const { SESSION_SECRET } = require('./config/session.json');
 
 const app = next({ dev });
 const handle = app.getRequestHandler();
@@ -13,13 +16,24 @@ app.prepare()
   .then(() => {
     const server = express();
 
+    server.use(bodyParser.json());
+    server.use(session({
+      secret: SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+    }));
+    server.use(passport.initialize());
+    server.use(passport.session());
+
+    server.use('/api', routes);
+
     server.get('*', (req, res) => {
       return handle(req, res);
     });
 
     server.listen(3000, (err) => {
       if (err) throw err;
-      // db.sequelize.sync();
+      db.sequelize.sync();
       console.log('Listening on http://localhost:3000');
     })
   })
