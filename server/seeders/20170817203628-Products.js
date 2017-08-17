5771f5f12a1e1963aaafa6582518a0d7f35b301a
nwre87 <nwre87@gmail.com>
2017-08-17 11:10:41 -1000

    add basic product seed and fixture

---
+++ server/seeders/20170817203628-Products.js
@@ -0,0 +1,10 @@
+'use strict';
+const { products } = require('../fixtures/products');
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Products', products, {});
+  },
+
+  down: function (queryInterface, Sequelize) {}
+};
