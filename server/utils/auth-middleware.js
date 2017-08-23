aa6222c6ea06688a471230b92aa0d777178e6102
nwre87 <nwre87@gmail.com>
2017-08-22 17:19:13 -1000

    add auth route protection middleware

---
+++ server/utils/auth-middleware.js
@@ -0,0 +1,18 @@
+const isAuthenticated = (req, res, next) => {  
+  if (req.isAuthenticated()) return next();
+    res
+    .status(401)
+    .json({ error: 'Unauthorized' });
+};
+
+const isAdmin = (req, res, next) => {
+  if (req.user.isAdmin) return next();
+  res
+  .status(401)
+  .json({ error: 'Unauthorized - not Admin' });
+};
+
+module.exports = {
+  isAuthenticated,
+  isAdmin,
+};
