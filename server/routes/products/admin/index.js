c82898ade59cc1a0d5872045eb46edba8ebae322
nwre87 <nwre87@gmail.com>
2017-08-24 21:05:41 -1000

    add admin/product/new route

--- server/routes/products/admin/index.js
+++ server/routes/products/admin/index.js
@@ -4,7 +4,34 @@ const { isAdmin } = require('../../../utils/auth-middleware');
 const router = express.Router();
 
 router.post('/new', isAdmin, (req, res) => {
-  res.send('NEW');
+  const {
+    title,
+    description,
+    inventory,
+    price,
+    category_id,
+  } = req.body;
+
+  const payload = {
+    title,
+    description,
+    inventory,
+    price,
+    category_id
+  };
+  
+  Product.forge(payload)
+  .save()
+  .then(product => {
+    res
+    .status(200)
+    .json({ product });
+  })
+  .catch(err => {
+    res
+    .status(500)
+    .json({ err });
+  })
 });
 
 router.put('/:productId/update', isAdmin, (req, res) => {
