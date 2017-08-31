abaf8ae90a64f9921add8b669eb7e4d8bdde46c4
nwre87 <nwre87@gmail.com>
2017-08-30 15:30:18 -1000

    single product page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Link from 'next/link';
 import { Provider } from 'mobx-react';
 import { Layout, Products } from '../components';
 import { initProductStore } from '../store/product';
---
+++ pages/singleProduct.js
@@ -0,0 +1,15 @@
+import React,  { Component } from 'react';
+
+class SingleProduct extends Component {
+  static getInitialProps() {
+
+  }
+
+
+
+  render() {
+    return (
+      <div>Single Product</div>
+    );
+  }
+}
