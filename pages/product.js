c61440ca2a15794c4d63f588dd3c5b83d37af686
nwre87 <nwre87@gmail.com>
2017-09-26 15:21:08 -1000

    add getInitialProps to product page

--- pages/product.js
+++ pages/product.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import { initProductStore } from '../store/product';
 
 class Product extends Component {
-  static getInitialProps() {
-    return {}
+  static async getInitialProps({ req }) {
+    const isServer = !!req;
+    const store = initProductStore(isServer);
+    await store.fetchSingleProduct();
+    return {
+      product: store.product,
+      error: store.error,
+      isServer,
+    }
   }
 
   render() {
+    constructor(props) {
+      super(props);
+      this.store = initProductStore(
+        props.isServer,
+        props.products, 
+        props.error
+      );
+    }
+
     return (
       <div>Single Product</div>
     );
