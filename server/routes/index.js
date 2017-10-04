8d9e428fd06dd14d048b8d422acde87df6e8ae29
nwre87 <nwre87@gmail.com>
2017-10-04 13:06:51 -1000

    stub out cart SS folder structure

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.use('/auth', require('./auth'));
 router.use('/products', require('./products'));
+router.use('/cart', require('./cart'))
 
 module.exports = router;
